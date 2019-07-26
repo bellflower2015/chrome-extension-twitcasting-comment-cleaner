723ccf1a624ee060971d153d1d61dfe4088b6f5c
Kikyou Akino <bellflower@web4u.jp>
2019-07-27 08:08:20 +0900

    Modify to find delete tag within new DOM structure

--- proj/script.js
+++ proj/script.js
@@ -5,4 +5,7 @@ $(function(){
     $('div#comment-list-app div.tw-player-page__comment__list div.tw-comment-item[data-type="owner"] div.tw-comment-popup ul.tw-popup-menu li[role="presentation"] a[role="menuitem"]:contains("削除")').each(function(i, elem){
         $(elem)[0].click();
     });
+    $('div#comment-list-app div.tw-player-page__comment__list div.tw-comment-item[data-type="you"] div.tw-comment-popup ul.tw-popup-menu li[role="presentation"] a[role="menuitem"]:contains("削除")').each(function(i, elem){
+        $(elem)[0].click();
+    });
 });
